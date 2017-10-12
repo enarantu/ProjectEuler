e027d1dfa431011820f725635c497053fd29236d
Enkhtur Narantungalag <enarantu@umich.edu>
2017-10-12 16:35:57 -0400

    solved problem 27

--- problems/p0027.cpp
+++ problems/p0027.cpp
@@ -5,9 +5,37 @@
 
 using namespace std;
 
+long compute(int a, int b, int n);
+int num_of_primes(int a, int b);
 int main(){
-	for(int i = 1; i < 200 ; i++){
-		cout << setw(6) << i << setw(5) << is_prime(i) << endl;
+	int max_primes = 0;
+	int product = 0;
+	for(int a = -999; a <= 999; a++){
+		for(int b = -1000; b <= 1000; b++){
+			int new_n = num_of_primes(a,b);
+			if(max_primes < new_n){
+				max_primes = new_n;
+				product = a*b;
+			}
+		}
 	}
+	cout << max_primes << " " << product<< endl;
 	return 0;
+}
+
+long compute(int a, int b, int n){
+	return n*n + a*n + b;
+}
+int num_of_primes(int a, int b){
+	int count = 0;
+	int p = 1,n = 0;
+	do{
+		p = compute(a,b,n);
+		n++;
+		count++;
+		if(p < 1){
+			p = 1;
+		}
+	}while(is_prime(p));
+	return count - 1;
 }
